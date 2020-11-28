2e402f115f2dad1d6c8a7f789cea4fc556a6f72b
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-11-28 10:37:51 +0100

    Fix indentation

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Disc Stalker - FRx
+
 TO - project
+
 # Contributors
+
 <table>
   <tr>
     <td align="center"><a href="https://github.com/surjak"><sub><b>Mateusz Surjak</b></sub></a><br /></td>
-  <td align="center"><a href="https://github.com/jonatanklosko"><sub><b>Jonatan Kłosko</b></sub></a><br /></td>
-  <td align="center"><a href="https://github.com/nazkord"><sub><b>Nazar Kurdiumov</b></sub></a><br /></td>
-  <td align="center"><a href="https://github.com/beneckimateusz"><sub><b>Mateusz Benecki</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/jonatanklosko"><sub><b>Jonatan Kłosko</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/nazkord"><sub><b>Nazar Kurdiumov</b></sub></a><br /></td>
+    <td align="center"><a href="https://github.com/beneckimateusz"><sub><b>Mateusz Benecki</b></sub></a><br /></td>
   </tr>
 </table>
 
 ## GGL docs
+
 [Click](https://docs.google.com/document/d/1tPEjTxmNRnkyuUooXjdKY55RkvUr_wm1BR9fZz1xevY/edit?usp=sharing)
--- src/main/java/com/frx/disc/stalker/App.java
+++ src/main/java/com/frx/disc/stalker/App.java
@@ -13,26 +13,26 @@ import javafx.stage.Stage;
  */
 //quick test if all works
 public class App extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Hello World!");
+    Button btn = new Button();
+    btn.setText("Say 'Hello World'");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+      @Override
+      public void handle(ActionEvent event) {
+        System.out.println("Hello World!");
+      }
+    });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    primaryStage.setScene(new Scene(root, 300, 250));
+    primaryStage.show();
+  }
 }
