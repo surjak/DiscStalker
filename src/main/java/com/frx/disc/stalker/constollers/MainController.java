72ebf14b6c42c6166ab571a69be55b78f42a4f39
Mateusz Surjak <surjak1999@gmail.com>
2020-11-30 10:23:48 +0100

    lekki refactor

--- src/main/java/com/frx/disc/stalker/constollers/MainController.java
+++ src/main/java/com/frx/disc/stalker/constollers/MainController.java
@@ -11,7 +11,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Created by surja on 29.11.2020
@@ -47,11 +46,14 @@ public class MainController {
         TreeItem rootItem = new TreeItem(root);
         fillTreeTable(rootItem, root);
         treeTableView.setRoot(rootItem);
+        refreshTreeOnChangeEvent(directoryTree);
+    }
+
+    private void refreshTreeOnChangeEvent(LiveDirectoryTree directoryTree) {
         directoryTree.getEventSubject()
                 .subscribeOn(Schedulers.io())
                 .observeOn(JavaFxScheduler.platform())
                 .subscribe(directoryWatcherEvent -> {
-                    System.out.println("aaa");
                     treeTableView.refresh();
                 });
     }
@@ -60,39 +62,50 @@ public class MainController {
     private void fillTreeTable(TreeItem treeItem, FileSystemNode systemNode) {
         if (systemNode.isDirectory()) {
             DirectoryNode directoryNode = (DirectoryNode) systemNode;
-            directoryNode.getChildNodes().forEach(child -> {
-                TreeItem subTree = new TreeItem(child);
-
-                treeItem.getChildren().add(subTree);
-                fillTreeTable(subTree, child);
-            });
+            fillSubTree(treeItem, directoryNode);
             directoryNode.getChildNodes().addListener((ListChangeListener<FileSystemNode>) change -> {
                 while (change.next()){
                     if(change.wasAdded()){
-                        change.getAddedSubList().forEach(systemNode1 -> {
-                            TreeItem subTree = new TreeItem(systemNode1);
-                            treeItem.getChildren().add(subTree);
-                            fillTreeTable(subTree, systemNode1);
-                        });
+                        handleCreateChange(treeItem, change);
                     }
-
                     if(change.wasRemoved()){
-                        change.getRemoved().forEach(t->{
-                            final List<TreeItem<FileSystemNode>> itemsToRemove = new ArrayList<>();
-
-                            for (Object child : treeItem.getChildren()) {
-                                TreeItem treeItem1 = (TreeItem) child;
-                                if (treeItem1.getValue().equals(t)){
-                                    itemsToRemove.add(treeItem1);
-                                }
-
-                            }
-
-                            treeItem.getChildren().removeAll(itemsToRemove);
-                        });
+                        handleRemoveChange(treeItem, change);
                     }
                 }
             });
         }
     }
+
+    private void fillSubTree(TreeItem treeItem, DirectoryNode directoryNode) {
+        directoryNode.getChildNodes().forEach(child -> {
+            TreeItem subTree = new TreeItem(child);
+
+            treeItem.getChildren().add(subTree);
+            fillTreeTable(subTree, child);
+        });
+    }
+
+    private void handleCreateChange(TreeItem treeItem, ListChangeListener.Change<? extends FileSystemNode> change) {
+        change.getAddedSubList().forEach(systemNode1 -> {
+            TreeItem subTree = new TreeItem(systemNode1);
+            treeItem.getChildren().add(subTree);
+            fillTreeTable(subTree, systemNode1);
+        });
+    }
+
+    private void handleRemoveChange(TreeItem treeItem, ListChangeListener.Change<? extends FileSystemNode> change) {
+        change.getRemoved().forEach(t->{
+            final List<TreeItem<FileSystemNode>> itemsToRemove = new ArrayList<>();
+
+            for (Object child : treeItem.getChildren()) {
+                TreeItem treeItem1 = (TreeItem) child;
+                if (treeItem1.getValue().equals(t)){
+                    itemsToRemove.add(treeItem1);
+                }
+            }
+
+            treeItem.getChildren().removeAll(itemsToRemove);
+        });
+    }
+
 }
