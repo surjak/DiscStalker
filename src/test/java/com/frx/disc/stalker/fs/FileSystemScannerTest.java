a74f4a4d3a7e516594ccf5933a449f09ac6dfaf9
Mateusz Surjak <surjak1999@gmail.com>
2020-12-02 12:27:31 +0100

    troche testow

---
+++ src/test/java/com/frx/disc/stalker/fs/FileSystemScannerTest.java
@@ -0,0 +1,109 @@
+package com.frx.disc.stalker.fs;
+
+import com.frx.disc.stalker.model.FileSystemNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by surja on 02.12.2020
+ */
+class FileSystemScannerTest {
+
+    FileSystemScanner scanner;
+    Consumer consumer;
+
+    @BeforeEach
+    void setUp() {
+        consumer = mock(Consumer.class);
+        scanner = new FileSystemScanner(consumer);
+    }
+
+    @Test
+    void givenDirectoryPathWhenScanDirectoryThenConsumerAcceptIsCalled() throws IOException {
+        //given
+        Path path = Paths.get("TEST_PATH");
+        mockStatic(Files.class);
+
+        //when
+        when(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(true);
+        when(Files.list(path)).thenReturn(Stream.empty());
+        scanner.scanRecursively(path);
+
+        //then
+        verify(consumer, times(1)).accept(any());
+    }
+
+    @Test
+    void givenFilePathWhenScanDirectoryThenConsumerAcceptIsCalled() throws IOException {
+        //given
+        Path path = Paths.get("TEST_PATH");
+        mockStatic(Files.class);
+
+        //when
+        when(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(true);
+        when(Files.list(path)).thenReturn(Stream.empty());
+        Optional<FileSystemNode> fileSystemNode = scanner.scanRecursively(path);
+
+        //then
+        assertTrue(fileSystemNode.get().isDirectory());
+    }
+
+    @Test
+    void givenDirectoryPathWhenScanDirectoryThenFileNodeIsReturnedAndHasProperSize() throws IOException {
+        //given
+        Path path = Paths.get("TEST_PATH");
+        mockStatic(Files.class);
+
+        //when
+        when(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(false);
+        when(Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(true);
+        when(Files.size(path)).thenReturn(5L);
+        Optional<FileSystemNode> fileSystemNode = scanner.scanRecursively(path);
+
+        //then
+        assertFalse(fileSystemNode.get().isDirectory());
+    }
+
+    @Test
+    void givenFilePathWhenScanDirectoryThenFileNodeIsReturnedAndHasProperSize() throws IOException {
+        //given
+        Path path = Paths.get("TEST_PATH");
+        mockStatic(Files.class);
+
+        //when
+        when(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(false);
+        when(Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(true);
+        when(Files.size(path)).thenReturn(5L);
+        Optional<FileSystemNode> fileSystemNode = scanner.scanRecursively(path);
+
+        //then
+        assertFalse(fileSystemNode.get().isDirectory());
+    }
+    @Test
+    void givenUnexpectedInputPathWhenScanDirectoryThenEmptyOptionalIsReturned() throws IOException {
+        //given
+        Path path = Paths.get("TEST_PATH");
+        mockStatic(Files.class);
+
+        //when
+        when(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(false);
+        when(Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)).thenReturn(false);
+        Optional<FileSystemNode> fileSystemNode = scanner.scanRecursively(path);
+
+        //then
+        assertTrue(fileSystemNode.isEmpty());
+    }
+
+}
---
+++ src/test/java/com/frx/disc/stalker/model/DirectoryNodeTest.java
@@ -0,0 +1,107 @@
+package com.frx.disc.stalker.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by surja on 02.12.2020
+ */
+class FileSystemNodeTest {
+
+    DirectoryNode directoryNode;
+
+    @BeforeEach
+    public void initDirectory(){
+        Path path = Paths.get("PATH");
+        directoryNode = new DirectoryNode(path);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddDirectoryAsChildThenChildNumberIncreases(){
+        //given
+        Path path = Paths.get("PATH");
+        DirectoryNode childNode = new DirectoryNode(path);
+
+        //when
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getChildNodes().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddDirectoryAsChildThenDirectorySizeIncreases(){
+        //given
+        Long EXPECTED_SIZE = 50L;
+        Path path = Paths.get("PATH");
+        DirectoryNode childNode = spy(new DirectoryNode(path));
+
+        //when
+        when(childNode.getSize()).thenReturn(EXPECTED_SIZE);
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getSize()).isEqualTo(EXPECTED_SIZE);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddDirectoryAsChildThenNumberOfFilesDoesNotIncrease(){
+        //given
+        Path path = Paths.get("PATH");
+        DirectoryNode childNode = new DirectoryNode(path);
+
+        //when
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getNumberOfFiles()).isEqualTo(0L);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddFileAsChildThenChildNumberIncreases(){
+        //given
+        Path path = Paths.get("PATH");
+        FileNode childNode = new FileNode(path, 10L);
+
+        //when
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getChildNodes().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddFileAsChildThenDirectorySizeIncreases(){
+        //given
+        Long EXPECTED_SIZE = 50L;
+        Path path = Paths.get("PATH");
+        FileNode childNode = spy(new FileNode(path, EXPECTED_SIZE));
+
+        //when
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getSize()).isEqualTo(EXPECTED_SIZE);
+    }
+
+    @Test
+    public void givenDirectoryNodeWhenAddFileAsChildThenNumberOfFilesIncrease(){
+        //given
+        Path path = Paths.get("PATH");
+        FileNode childNode = new FileNode(path, 10L);
+
+        //when
+        directoryNode.addChild(childNode);
+
+        //then
+        assertThat(directoryNode.getNumberOfFiles()).isEqualTo(1L);
+    }
+}
