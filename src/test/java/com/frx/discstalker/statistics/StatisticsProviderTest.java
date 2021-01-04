fc26c79154c586f65f634b82cb0944bec745c6d2
nazkord <nazkord@gmail.com>
2021-01-04 22:26:53 +0100

    add dynamically management of calculators

--- src/test/java/com/frx/discstalker/statistics/StatisticsProviderTest.java
+++ src/test/java/com/frx/discstalker/statistics/StatisticsProviderTest.java
@@ -2,13 +2,19 @@ package com.frx.discstalker.statistics;
 
 import com.frx.discstalker.fs.LiveDirectoryTree;
 import com.frx.discstalker.model.DirectoryNode;
+import com.frx.discstalker.statistics.concreteStatistics.StatisticCalculator;
+import com.frx.discstalker.statistics.concreteStatistics.directoryStatistics.DirectoryStatistics;
+import com.frx.discstalker.statistics.concreteStatistics.directoryStatistics.DirectoryStatisticsCalculator;
 import com.frx.discstalker.statistics.concreteStatistics.directoryStatistics.PercentageUsageOfAllowedSpace;
 import com.frx.discstalker.statistics.concreteStatistics.filesStatistics.FileSize;
+import com.frx.discstalker.statistics.concreteStatistics.filesStatistics.FileStatisticsCalculator;
 import com.frx.discstalker.statistics.concreteStatistics.filesStatistics.NLargestFiles;
 import com.frx.discstalker.statistics.concreteStatistics.filesStatistics.NumberOfFiles;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -28,7 +34,7 @@ class StatisticsProviderTest {
     when(directoryTree.getRoot()).thenReturn(rootNode);
     when(rootNode.isDirectory()).thenReturn(false);
     when(directoryTree.getEventSubject()).thenReturn(PublishSubject.create());
-    StatisticsProvider statisticsProvider = new StatisticsProvider(directoryTree);
+    StatisticsProvider statisticsProvider = new StatisticsProvider(directoryTree, List.of(new FileStatisticsCalculator(), new DirectoryStatisticsCalculator()));
 
     //then
     assertThat(statisticsProvider.getStatisticList()).hasSize(4);
@@ -38,4 +44,27 @@ class StatisticsProviderTest {
     assertThat(statisticsProvider.getStatisticList()).anyMatch(statistic -> statistic.getClass().equals(PercentageUsageOfAllowedSpace.class));
   }
 
+  @Test
+  void givenStatisticProviderWhenSetNewCalculatorThenNewStatisticsAreInList() {
+    //given
+    LiveDirectoryTree directoryTree = mock(LiveDirectoryTree.class);
+    DirectoryNode rootNode = mock(DirectoryNode.class);
+    DirectoryStatistics oldDirectoryStatistics = new PercentageUsageOfAllowedSpace(200L);
+    DirectoryStatistics newDirectoryStatistics = new PercentageUsageOfAllowedSpace(400L);
+    StatisticCalculator oldStatisticCalculator = new DirectoryStatisticsCalculator(List.of(oldDirectoryStatistics));
+    StatisticCalculator newStatisticCalculator = new DirectoryStatisticsCalculator(List.of(newDirectoryStatistics));
+
+    //when
+    when(directoryTree.getRoot()).thenReturn(rootNode);
+    when(rootNode.isDirectory()).thenReturn(false);
+    when(directoryTree.getEventSubject()).thenReturn(PublishSubject.create());
+    StatisticsProvider statisticsProvider = new StatisticsProvider(directoryTree, List.of(oldStatisticCalculator));
+    List<? extends Statistic> statisticList = oldStatisticCalculator.getStatistics();
+    statisticsProvider.registerCalculator(newStatisticCalculator);
+
+    //then
+    assertThat(statisticsProvider.getStatisticList()).hasSameSizeAs(statisticList);
+    assertThat(statisticsProvider.getStatisticList()).doesNotContain(oldDirectoryStatistics);
+    assertThat(statisticsProvider.getStatisticList()).containsOnly(newDirectoryStatistics);
+  }
 }
